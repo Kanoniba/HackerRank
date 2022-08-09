834fe5f5bec90e5907face6b8aaf346a78d88ed1
Kano <kanoniba@gmail.com>
2022-08-09 17:48:51 +0800

    add 1 question

---
+++ src/Arrays/Array/Manipulation/Solution.java
@@ -0,0 +1,85 @@
+package Arrays.Array.Manipulation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+        // Write your code here
+        long[] array = new long[n+1];
+        int times = queries.size();
+        for (int i = 0; i < times; i++) {
+
+            List<Integer> list = queries.get(i);
+            int min = list.get(0) - 1;
+            int max = list.get(1);
+            int k = list.get(2);
+            array[min] += k;
+            array[max] -= k ;
+
+
+        }
+        long sum = 0;
+        long max = -999999;
+        for (int i = 0; i < n; i++) {
+            sum += array[i] ;
+            if (max < sum)
+                max = sum;
+        }
+        return max;
+    }
+
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> queriesRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowTempItems[j]);
+                queriesRowItems.add(queriesItem);
+            }
+
+            queries.add(queriesRowItems);
+        }
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
